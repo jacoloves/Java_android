d9e735a132915e0c226ec6e11afcfab43e0e3bfa
Shotaro Tanaka <5511068t@gmail.com>
2018-12-20 20:37:28 +0900

    20181220

--- MyApplication4/app/src/main/java/com/example/shotaro/myapplication4/MainActivity.java
+++ MyApplication4/app/src/main/java/com/example/shotaro/myapplication4/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.shotaro.myapplication4;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int HARE = 1;
+    private final int KUMORI = 2;
+    private final int AME = 3;
+    private final int KAZE = 4;
+    private final int ARASHI = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +32,27 @@ public class MainActivity extends AppCompatActivity {
                 animalImage.setImageResource(R.drawable.lion);
             }
         });
+
+        kasaIruka(AME);
+        kasaIruka(KUMORI);
+        kasaIruka(HARE);
+        kasaIruka(ARASHI);
+    }
+
+    private void kasaIruka(int tenki){
+        switch (tenki){
+            case AME:
+                Log.d("行動", "傘を持っていく");
+                break;
+            case KUMORI:
+                Log.d("行動", "折り畳み傘を持っていく");
+                break;
+            case HARE:
+                Log.d("行動", "傘を持っていかない");
+                break;
+                default:
+                    Log.d("行動", "わかりません");
+
+        }
     }
 }
