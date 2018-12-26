98d70e1fc5f97609f517ffe0cfcdbdae94d39a77
jacoloves <5511068t@gmail.comn>
2018-12-26 23:28:00 +0900

    20181226

--- Slideshow/app/src/main/java/com/example/shotaro/slideshow/MainActivity.java
+++ Slideshow/app/src/main/java/com/example/shotaro/slideshow/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.shotaro.slideshow;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageSwitcher;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageSwitcher mImageSwither;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageSwither = (ImageSwitcher) findViewById(R.id.imageSwicher);
     }
 }
